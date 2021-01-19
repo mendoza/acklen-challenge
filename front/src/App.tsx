5bed0708e20b570fd511e0f70fe518540d5d1645
David Mendoza <d.mendoza99@unitec.edu>
2021-01-19 14:02:22 -0600

    add router types made basic pages made navbar compatible with router

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Container } from 'reactstrap';
 import NavBar from './components/NavBar';
+import Error404 from './views/Error404';
+import Home from './views/Home';
 
 const App = () => (
   <>
-    <NavBar />
+    <Router>
+      <NavBar />
+      <Container>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route>
+            <Error404 />
+          </Route>
+        </Switch>
+      </Container>
+    </Router>
   </>
 );
 
--- front/src/components/NavBar.tsx
+++ front/src/components/NavBar.tsx
@@ -1,16 +1,7 @@
 import React, { useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  NavbarText,
-  Button,
-} from 'reactstrap';
+import { Collapse, Navbar, NavbarToggler, Nav, NavItem, NavbarText, Button } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   const { loginWithRedirect, logout, user } = useAuth0();
@@ -19,19 +10,21 @@ const NavBar = () => {
 
   return (
     <Navbar color="dark" dark expand="md">
-      <NavbarBrand href="/">My Treasure</NavbarBrand>
+      <Link className="navbar-brand" to="/">
+        My Treasure
+      </Link>
       <NavbarToggler onClick={toggle} />
       <Collapse isOpen={isOpen} navbar>
         <Nav className="mr-auto" navbar>
           <NavItem>
-            <NavLink href="/collections/">My Collections</NavLink>
+            <Link className="nav-link" to="/collections/">
+              My Collections
+            </Link>
           </NavItem>
         </Nav>
         <NavbarText>
           {user ? (
-            <Button onClick={() => logout()}>
-              Log out <i className="fa fa-logout" />
-            </Button>
+            <Button onClick={() => logout()}>Log out</Button>
           ) : (
             <Button onClick={() => loginWithRedirect()}>Log in</Button>
           )}
