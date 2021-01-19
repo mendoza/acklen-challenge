6a30e43f1eb9fa998084b858a3557d36d2eac436
David Mendoza <d.mendoza99@unitec.edu>
2021-01-19 15:09:36 -0600

    made some placeholders for the information, but that should be good for now

--- front/src/views/Error404.tsx
+++ front/src/views/Error404.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Jumbotron } from 'reactstrap';
 
 const Error404 = () => (
-  <div>
-    <p>Error</p>
-  </div>
+  <Jumbotron>
+    <h1 className="display-3">Error 404!</h1>
+    <p className="lead">Well this is awkward...</p>
+    <hr className="my-2" />
+    <p>Either we did an error or you did...</p>
+  </Jumbotron>
 );
 
 export default Error404;
--- front/src/views/Home.tsx
+++ front/src/views/Home.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import { Col, Row } from 'reactstrap';
 
 const Home = () => (
-  <div>
-    <p>hello</p>
+  <div id="front-page">
+    <Row>
+      <Col>
+        <h1>Welcome to My Treasure</h1>
+        <p>
+          The web app that lets you control your collections, tells you the total price for your
+          collection and even share items
+        </p>
+        <h2>Why should you use this app?</h2>
+        <ol>
+          <li>Open source</li>
+          <li>All your data is safe</li>
+          <li>once you delete your info from our app is gone for real</li>
+        </ol>
+      </Col>
+      <Col>
+        <img src="/assets/treasure.png" id="front-page-logo" alt="Logo" />
+      </Col>
+    </Row>
   </div>
 );
 
