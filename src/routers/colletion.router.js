b92330b13f27e6c79390b538d5dba13524136343
David Mendoza <d.mendoza99@unitec.edu>
2021-01-19 20:10:41 -0600

    made a finder for the users collections

--- src/routers/colletion.router.js
+++ src/routers/colletion.router.js
@@ -1,6 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const DefaultError = require('../middlewares/defaultError.middleware');
+const ValidateKey = require('../middlewares/validateKey.middleware');
+const Collections = require('../models/collection.model');
 
 const collection = express.Router();
 
+collection.use(ValidateKey);
 
+collection.get('/:userId', (req, res, next) => {
+  const { userId } = req.params;
+  if (userId && userId !== '') {
+    Collections.find({ user: mongoose.Types.ObjectId(userId) })
+      .then((data) => {
+        res.status(200).json({ success: true, collections: data });
+      })
+      .catch(next);
+  }
+});
+
+collection.use(DefaultError);
 module.exports = collection;
