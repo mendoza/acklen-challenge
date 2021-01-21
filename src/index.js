6129ee5f6e27a91b3c05daa9908d77dd0bb2c8a5
David Mendoza <d.mendoza99@unitec.edu>
2021-01-20 22:25:07 -0600

    unused code...

--- src/index.js
+++ src/index.js
@@ -44,16 +44,9 @@ const accessLogStream = fs.createWriteStream(
 );
 app.use(morgan('tiny', { stream: accessLogStream }));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('front/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '..', 'front', 'build', 'index.html'));
-  });
-}
-
 app.use('/api', api);
-api.use(DefaultError);
-api.use(NotFound);
+app.use(DefaultError);
+app.use(NotFound);
 
 app.listen(PORT, () => {
   console.log(`Server started on port: ${PORT}`);
